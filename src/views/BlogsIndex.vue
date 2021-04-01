2bbddae9e30ece4f9d933ec9961bf8a2ee0901e8
Eric <lee.erictae@gmail.com>
2021-03-31 21:01:57 -0500

    add login,logout,signup,idex routes and views

---
+++ src/views/BlogsIndex.vue
@@ -0,0 +1,38 @@
+  
+<template>
+  <div class="blogs-index">
+    <!-- recipes.each do |recipe| -->
+    <div v-for="blog in blogs">
+      {{ blog.title }}
+      <hr >
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Recipes index page",
+      blogs: [],
+    };
+  },
+  created: function () {
+    this.blogsIndex();
+  },
+  methods: {
+    blogsIndex: function () {
+      console.log("on the index paage...");
+      // make a web request to rails to get all the recipes
+      axios.get("/api/blogs").then((response) => {
+        console.log(response.data);
+        this.blogs = response.data;
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/Sample.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="test">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Vue.js! test page",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
