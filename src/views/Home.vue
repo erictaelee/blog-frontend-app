2e82c78c56a8be994b25c8675f6af2bb5e3ce06e
Eric <lee.erictae@gmail.com>
2021-04-06 18:39:14 -0500

    add index method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>{{ posts }}</h1>
 
   </div>
 </template>
@@ -9,13 +10,26 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      posts: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      console.log("in posts index...");
+      axios.get("http://localhost:3000/api/posts").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
 };
 </script>
